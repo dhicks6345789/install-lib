374e881a02f69c4b325df1fd3d0475bbbc14ce90
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-03-06 14:10:03 +0000

    Update installLib.py

--- installLib.py
+++ installLib.py
@@ -12,7 +12,7 @@ def writeFile(theFilename, theFileData):
 		fileDataHandle.write(theFileData.encode())
 	else:
 		for dataLine in theFileData:
-			fileDataHandle.write((dataLine + "\n").encode())
+			fileDataHandle.write((str(dataLine) + "\n").encode())
 	fileDataHandle.close()
 	
 def runCommand(theCommand):
